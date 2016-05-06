0a774651fc90e1b788c4fa521052c6d79861bb9d
phuang <n25672729@gamil.com>
2016-05-06 22:32:42 +0800

    new file:   MRT.xcodeproj/project.pbxproj 	new file:   MRT.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   MRT.xcodeproj/project.xcworkspace/xcuserdata/Niki.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   MRT.xcodeproj/xcuserdata/Niki.xcuserdatad/xcschemes/MRT.xcscheme 	new file:   MRT.xcodeproj/xcuserdata/Niki.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   MRT.xcworkspace/contents.xcworkspacedata 	new file:   MRT.xcworkspace/xcuserdata/Niki.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   MRT.xcworkspace/xcuserdata/Niki.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist 	new file:   MRT/AppDelegate.swift 	new file:   MRT/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   MRT/Base.lproj/LaunchScreen.storyboard 	new file:   MRT/Base.lproj/Main.storyboard 	new file:   MRT/Info.plist 	new file:   MRT/MRTStationsListTableViewController.swift 	new file:   MRT/ViewController.swift 	new file:   Podfile 	new file:   Podfile.lock 	new file:   Pods/Manifest.lock 	new file:   Pods/ObjectMapper/LICENSE 	new file:   Pods/ObjectMapper/ObjectMapper/Core/FromJSON.swift 	new file:   Pods/ObjectMapper/ObjectMapper/Core/Map.swift 	new file:   Pods/ObjectMapper/ObjectMapper/Core/Mappable.swift 	new file:   Pods/ObjectMapper/ObjectMapper/Core/Mapper.swift 	new file:   Pods/ObjectMapper/ObjectMapper/Core/Operators.swift 	new file:   Pods/ObjectMapper/ObjectMapper/Core/ToJSON.swift

---
+++ MRT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRT
+//
+//  Created by Ｎiki25672729 on 5/3/16.
+//  Copyright © 2016 Niki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
